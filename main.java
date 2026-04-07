7c45518e812aa2855cb186ae4185774eb78a25ac
rudramaheta71 <acpxyz31@gmail.com>
2026-04-07 15:44:09 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+public static void main(String[] args){
+int n=15;
+if(n%3==0 && n%5==0){
+System.out.println("Divisible by both");
+}else{
+System.out.println("not divisible by both");}
+}} 
